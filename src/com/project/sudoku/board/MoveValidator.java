66026d632cdd2a5554bb8ef7d80d7903b6750aea
Raul Abanto <raulanthony.abanto@cit.edu>
2026-03-03 00:11:30 +0800

    feat: implement MoveValidator class

--- src/com/project/sudoku/board/MoveValidator.java
+++ src/com/project/sudoku/board/MoveValidator.java
@@ -1,21 +1,26 @@
 package com.project.sudoku.board;
 
 public class MoveValidator {
-    //Suggest nako mao ni imo una buhaton :) kay dali ra
+    public boolean isValidMove(int[][]board, int row, int col, int num){
+        int GRIZ_SIZE = board.length;
+        //checking the row
+        for(int j = 0;j < GRIZ_SIZE;j++){
+            if(board[row][j] == num) return false;
+        }
 
-    /*
-     * isValidMove()
-     * -------------
-     * Checks if a number can be placed at a specific cell.
-     *
-     * Parameters:
-     * - board: the 2D Sudoku board
-     * - row: row index (0–8)
-     * - col: column index (0–8)
-     * - num: number to place (1–9)
-     *
-     * Returns:
-     * - true if the move is valid
-     * - false if the number already exists in the row, column, or box
-     */
+        //checking the column
+        for(int i = 0;i < GRIZ_SIZE;i++){
+            if(board[i][col] == num) return false;
+        }
+
+        //checking the box
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for(int i = startRow;i < startRow + 3;i++){
+            for(int j = startCol;j < startCol + 3;j++){
+                if(board[i][j] == num) return false;
+            }
+        }
+        return true;
+    }
 }
