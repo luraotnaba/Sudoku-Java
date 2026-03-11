4f1b4dfd809506317b978924a917b568bbfc0d38
Raul Abanto <raulanthony.abanto@cit.edu>
2026-03-12 01:01:19 +0800

    // fix: added try-catch to prevent InputMismatchException when a user

--- src/com/project/sudoku/game/InputHandler.java
+++ src/com/project/sudoku/game/InputHandler.java
@@ -11,13 +11,23 @@ public class InputHandler {
     }
 
     public int getMenuChoice() {
-        System.out.println("Options:");
-        System.out.println("1 - Enter a move");
-        System.out.println("2 - Get a hint");
-        System.out.println("3 - Show solved board");
-        System.out.println("0 - Exit");
-        System.out.print("Choice: ");
-        return s.nextInt();
+        while (true) {
+            try {
+                System.out.println("Options:");
+                System.out.println("1 - Enter a move");
+                System.out.println("2 - Get a hint");
+                System.out.println("3 - Show solved board and player stats");
+                System.out.println("0 - Exit");
+                System.out.print("Choice: ");
+
+                int choice = s.nextInt();
+                return choice;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input! Please enter a number.");
+                s.nextLine(); // clear invalid input
+            }
+        }
     }
 
     public int getRow() {
